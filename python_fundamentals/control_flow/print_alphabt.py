55d72d3abdcdac2c1f77274b09a1cb6b1aad382d
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-16 10:40:45 +0300

    Fix format print for print_alphabt

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
-print("".join(chr(i) for i in range(97, 123) if chr(i) not in "qe"))
+for i in range(97, 123):
+    if chr(i) not in "qe":
+        print("{}".format(chr(i)), end="")
+print()
