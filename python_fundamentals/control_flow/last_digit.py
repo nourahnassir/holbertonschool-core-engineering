8c9aaa56d94826934f52ace2b4014808912be5ed
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-16 10:32:26 +0300

    Shorten code to fix pycodestyle length limit

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+ld = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit
+    ld = -ld
 
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+msg = f"Last digit of {number} is {ld} and is"
+if ld > 5:
+    print(f"{msg} greater than 5")
+elif ld == 0:
+    print(f"{msg} 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0" )
+    print(f"{msg} less than 6 and not 0")
