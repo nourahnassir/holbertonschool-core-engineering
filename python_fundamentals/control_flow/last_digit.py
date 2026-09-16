b824341741fa3bb6924ee3f79486b3fa67444318
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-16 10:24:25 +0300

    Fix pycodestyle errors in last_digit.py

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
+
 last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
@@ -9,4 +10,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(  f"Last digit of {number} is {last_digit} "
+        "and is less than 6 and not 0" )
