d7513c3bd386f911c45ea37ca415899a1b37fae3
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-16 11:07:16 +0300

    Fix exact output length and style for print_alphabt

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-print("".join(["{}".format(chr(i)) for i in range(97, 123) if chr(i) not in "qe"]) + "\n")
+print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in "qe"))
