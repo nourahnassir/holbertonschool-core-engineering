d8bbbe6bde53b949f37f5856a1184fa5678eca6b
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-26 01:55:35 +0300

    Add 1-square.py

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a square by its size."""
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
