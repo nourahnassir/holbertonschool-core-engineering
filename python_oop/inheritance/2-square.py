7b53eb20314450837585a232c0a340b9befa6e2f
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-09-26 02:39:48 +0300

    Add string representation to 2-square.py

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines a class Square that inherits from Rectangle."""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns the print and str representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
